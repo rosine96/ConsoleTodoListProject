0fdd1483ed0673390008e980c92f84035cd55ed0
rosine ngono <ngrosine07@gmail.com>
2024-04-16 03:54:41 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+namespace TodoList
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int operation;
+            int choix;
+            do
+            {
+                Console.WriteLine(@"Welcome to ToDoList!
+                                   what do you want?:
+                                   1-Add a task
+                                   2-Get your ToDoList
+                                   3-Delete a task
+                                   4-mark task as finish
+                                   5-quitter
+                                   ");
+                operation = int.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case 1:
+                        Console.WriteLine("entrer votre tache!");
+                        string tache = Console.ReadLine();
+                        Console.WriteLine("entrer l'heure prevu pour le debut de la tache!(format heure:min exemple 12:30)");
+                        string debut = Console.ReadLine();
+                        Console.WriteLine("entrer l'heure prevu pour la fin de la tache!(format heure:min exemple 12:30)");
+                        string fin = Console.ReadLine();
+                        Task myTask = new Task(tache, debut, fin);
+                        myTask.AddTask("task.txt");
+                        break;
+                    case 2:
+
+                        Task.GetMyTask("task.txt");
+
+                        break;
+                    case 3:
+                        Task.GetMyTask("task.txt");
+                        Console.WriteLine(@"--------Quelle tache voulez vous supprimer?--------(entrer le numero de la tache)");
+                        int numero = int.Parse(Console.ReadLine());
+                        Task.DeleteTask(numero, "task.txt");
+                        break;
+                    case 4:
+                        Task.GetMyTask("task.txt");
+                        Console.WriteLine(@"--------Quelle tache avez vous termine?--------(entrer le numero de la tache)");
+                        numero = int.Parse(Console.ReadLine());
+                        Task.FinishTask(numero, "task.txt");
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("veuillez choisir un numero parmis ceux de la liste\n");
+                        break;
+                }
+                Console.WriteLine(@"Avez vous une autre operation a effectuer?
+                                               1-oui
+                                               2-non");
+                 choix = int.Parse(Console.ReadLine());
+            } while (choix == 1);
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TodoList
+{
+    public class Task
+    {
+        public string? name { get; set; }
+        public bool done { get; set; } = false;
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public Task(string name,string startTime,string endTime)
+        {
+            this.name = name;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+        public void AddTask(string filepath) 
+        {
+            File.AppendAllText(filepath, $"{this.name}:           {this.startTime}-{this.endTime}"+Environment.NewLine);
+        }
+        public static void GetMyTask(string filepath)
+        {
+            try
+            {
+                string[] allTask = File.ReadAllLines(filepath);
+                for (int i = 0; i < allTask.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}-{allTask[i]}");
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+
+
+        }
+        public static void DeleteTask(int number, string filepath)
+        {
+            string[] allTask = File.ReadAllLines(filepath);
+            string[] newLines = new string[allTask.Length-1];
+           int j = 0;
+            for (int i = 0; i < allTask.Length; i++)
+            {
+                if(i !=number-1)
+                {
+                    newLines[j] = allTask[i];
+                    j++;
+                }
+                
+            }
+            File.Delete(filepath);
+            File.AppendAllLines(filepath, newLines);
+            Console.WriteLine($"Success deleted task");
+        }
+        public static void  FinishTask(int number, string filepath)
+        {
+            string[] allTask = File.ReadAllLines(filepath);
+            string[] newLines = new String[allTask.Length];
+            for (int i = 0; i < allTask.Length; i++)
+            {
+                if (i != number - 1)
+                {
+                    newLines[i] = allTask[i];
+                    
+                }
+                else
+                {
+                    newLines[i] = allTask[i]+"------finish---";
+                    
+                }
+
+            }
+            File.Delete(filepath);
+            File.AppendAllLines(filepath, newLines);
+        }
+       
+
+    }
+   
+}
